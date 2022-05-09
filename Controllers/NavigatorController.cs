90b33a2c0380be40c9abb4998c4f1e802b735bc9
Ro98h <52565633+Ro98h@users.noreply.github.com>
2022-05-09 19:39:00 +0200

    add navigatorController

---
+++ Controllers/NavigatorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    public class NavigatorController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
