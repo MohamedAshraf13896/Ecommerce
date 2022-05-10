58b9779bd5d099fe932e1dd934fd5e8f98c1b156
Omarelshazly14 <omarelshazly014@gmail.com>
2022-05-10 22:39:39 +0200

    Adding Identity Module Adding ApplicationUser Class Adding Account Controller

---
+++ Migrations/20220510182136_v2.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Project.Migrations
+{
+    public partial class v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "Shippers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Payments",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyName",
+                table: "Shippers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ ViewModels/RegisterVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required, MaxLength(256)]
+        public string UserName { get; set; }
+
+        [Required, MaxLength(256)]
+        public string Address { get; set; }
+
+        [Required,EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
