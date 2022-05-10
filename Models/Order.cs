3519e07cd63b27a72f1b99cebe997073c6f80d4e
Ro98h <52565633+Ro98h@users.noreply.github.com>
2022-05-10 23:18:29 +0200

    validation_v1

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,30 +1,75 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Project.Models
 {
+    [Index(nameof(Order.OrderNumber), IsUnique = true)]
     public class Order
     {
         public int ID { get; set; }
+
+
+        [Required(ErrorMessage = "Order Number is required")]
+        [Display(Name = "Order Number")]
         public int OrderNumber { get; set; }
+
+
+        [Required(ErrorMessage = "Address is required")]
+        [Display(Name = "Address")]
         public string Address { get; set; }
+
+
+        [Required(ErrorMessage = "Order Date is required")]
+        [Column(TypeName = "date")]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Order Date")]
         public DateTime OrderDate { get; set; }
+
+
+        [Required(ErrorMessage = "Ship Date is required")]
+        [Column(TypeName = "date")]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Ship Date")]
         public DateTime ShipDate { get; set; }
+
+
+        [Display(Name = "Sub Total")]
         public decimal SubTotal { get; set; }
-        public decimal Discount { get; set; }
+
+
+        [Display(Name = "Discount")]
+        public decimal Discount { get; set; } = 0;
+
+
+        [Required(ErrorMessage = "Order Tax is required")]
+        [Display(Name = "Order Tax")]
         public decimal OrderTax { get; set; }
+
+
+        [Display(Name = "Total Price")]
         public decimal TotalPrice { get; set; }
+
+
+        [Display(Name = "Is Paid")]
         public bool IsPaid { get; set; }
+
+
         //Foreign Keys
         [ForeignKey("Shipper")]
         public int ShipperID { get; set; }
 
         [ForeignKey("Payment")]
         public int PaymentID { get; set; }
+        [ForeignKey("Customer")]
+        public string CustomerId { get; set; }
+
         //Navigation Properties
         public Shipper Shipper { get; set; }
         public Payment Payment { get; set; }
+        public ApplicationUser Customer { get; set; }
         public virtual List<OrderDetails> OrderDetails { get; set; }
 
     }
--- Models/Payment.cs
+++ Models/Payment.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Project.Models
 {
     public class Payment
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Type is required")]
+        [Display(Name = "Payment Type")]
         public string Type { get; set; }
 
         //Foreign Keys
